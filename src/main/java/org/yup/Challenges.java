bcba09a6f192b3031a199ff96b554897b7fb616d
Santiago Osuna <santiago_osuna@icloud.com>
2023-04-17 15:59:51 -0400

    added several challenges

--- src/main/java/org/yup/Challenges.java
+++ src/main/java/org/yup/Challenges.java
@@ -1,4 +1,72 @@
 package org.yup;
 
+import java.util.Scanner;
 public class Challenges {
+
+
+    public String greeting(String name, int age) {
+        //let folks into the party if they are 21 and up
+        //politely tell them they cant enter under age
+        if (age >= 21) {
+            return "Hello " + name + ", welcome!";
+            //this is what happens if the condition is ture
+        } else {
+            return "Hello " + name + ", I am sorry but we cannot let you into the party";
+            //this is what happens if the condition is false
+        }
+
+    }
+
+
+    public String greetingWithUserInput() {
+        Scanner theScanner = new Scanner(System.in);
+
+        //We need to ask the user what their name is
+        System.out.print("Enter your name: ");
+        String name = theScanner.nextLine();
+
+        //We need to ask the user what their age is
+        System.out.print("Enter your age: ");
+        int age = theScanner.nextInt();
+
+        //let folks into the party if they are 21 and up
+        //politely tell them they cant enter under age
+        if (age >= 21) {
+            return "Hello " + name + ", welcome to the 21+ bar!";
+        } else {
+            return "Hello " + name + ", I am sorry but we cannot let you into the 21+ bar";
+        }
+
+    }
+
+
+    public int addTwoNumbers() {
+        Scanner theScanner = new Scanner(System.in);
+
+        //ask the user for number1
+        System.out.print("Enter number: ");
+        int number1 = theScanner.nextInt();
+
+        //ask the user for number2
+        System.out.print("Enter number: ");
+        int number2 = theScanner.nextInt();
+
+        //return the sum of number1 and number2 together
+        return number1 + number2;
+    }
+
+
+    public int subtractTwoNumbers() {
+        Scanner theScanner = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int number1 = theScanner.nextInt();
+
+        System.out.print("Enter number: ");
+        int number2 = theScanner.nextInt();
+
+        //return the diff of number1 and number 2 together
+        return number1 - number2;
+    }
+
 }
--- src/main/java/org/yup/JavaChallengesApp.java
+++ src/main/java/org/yup/JavaChallengesApp.java
@@ -1,10 +1,38 @@
 package org.yup;
 
+//Doing to show you an import but this is not required since both are in the same package
+//import org.yup.Challenges;
 public class JavaChallengesApp {
-
     public static void main(String[] args){
 
-        System.out.println("Hello form the Java Challenges App");
+
+        //creates a new instance of the challenges class so we can use the challenges in our application
+        Challenges challenges = new Challenges();
+
+
+        //this runs the greeting methods from our challenges and returns a string greeting and
+        //stores it in the greeting variable we created
+        String greeting = challenges.greeting("Guest",21);
+
+
+        //this prints the greeting in the console
+        System.out.println(greeting);
+
+
+        //code below is running greeting WithUserInput()
+        String greetingWithUserInput = challenges.greetingWithUserInput();
+        System.out.println(greetingWithUserInput);
+
+
+        //code below is running addTwoNumbers
+        int Sum = challenges.addTwoNumbers();
+        System.out.println(Sum);
+
+
+        //code below is running subtractTwoNumbers
+        int Subtract = challenges.subtractTwoNumbers();
+        System.out.println(Subtract);
+
     }
 
 }
